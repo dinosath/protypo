14ae4e8b32fe9adbc8c372f68baa630bdb62c336
Konstantinos Athanasiou <dinosath0@gmail.com>
2024-12-09 21:36:32 +0200

    spring-rs: add many to many relationship to migration script

--- generators/spring-rs/files/src/main.rs
+++ generators/spring-rs/files/src/main.rs
@@ -1,6 +1,8 @@
 use spring::{auto_config, App};
 use spring_sea_orm::{SeaOrmPlugin,};
 use spring_web::{WebConfigurator, WebPlugin};
+use spring_opentelemetry::{KeyValue, OpenTelemetryPlugin, ResourceConfigurator, SERVICE_NAME, SERVICE_VERSION, };
+
 mod models;
 mod controllers;
 
@@ -8,8 +10,13 @@ mod controllers;
 #[tokio::main]
 async fn main() {
     App::new()
+        .opentelemetry_attrs([
+            KeyValue::new(SERVICE_NAME, env!("CARGO_PKG_NAME")),
+            KeyValue::new(SERVICE_VERSION, env!("CARGO_PKG_VERSION")),
+        ])
         .add_plugin(SeaOrmPlugin)
         .add_plugin(WebPlugin)
+        .add_plugin(OpenTelemetryPlugin)
         .run()
         .await
 }
