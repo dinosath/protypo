cc706c61ba7b7cde90485f3cf6e7d284d03cdcb6
Konstantinos Athanasiou <dinosath0@gmail.com>
2025-03-02 23:32:46 +0200

    fix loco multitenancy

--- generators/loco/files/src/app.rs
+++ generators/loco/files/src/app.rs
@@ -40,14 +40,13 @@ impl Hooks for App {
     }
 
     async fn initializers(_ctx: &AppContext) -> Result<Vec<Box<dyn Initializer>>> {
-        Ok(vec![Box::new(
-            initializers::view_engine::ViewEngineInitializer,
-        )])
+        Ok(vec![
+        ])
     }
 
     fn routes(_ctx: &AppContext) -> AppRoutes {
-        AppRoutes::with_default_routes() // controller routes below
-            .add_route(controllers::auth::routes())
+        AppRoutes::with_default_routes()
+            .add_route(controllers::auth::routes()) // controller routes below
     }
 
     async fn connect_workers(ctx: &AppContext, queue: &Queue) -> Result<()> {
