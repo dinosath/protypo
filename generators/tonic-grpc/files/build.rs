adf959ecca0dfb7427ca96b42043acb0995b8d2f
Konstantinos Athanasiou <dinosath0@gmail.com>
2025-03-07 20:08:17 +0200

    create fullstack grpc generator

---
+++ generators/tonic-grpc/files/build.rs
@@ -0,0 +1,6 @@
+use std::{env, path::PathBuf};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //Inject proto files into the build script here
+    Ok(())
+}
---
+++ generators/tonic-grpc/files/src/main.rs
@@ -0,0 +1,15 @@
+use tonic::transport::Server;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse().unwrap();
+
+    println!("Server listening on {}", addr);
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
