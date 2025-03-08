2be0321245976dbcfaacac3db58c090a6611a2df
Konstantinos Athanasiou <dinosath0@gmail.com>
2025-03-08 08:45:24 +0200

    add grpc api

--- generators/tonic-grpc/files/build.rs
+++ generators/tonic-grpc/files/build.rs
@@ -1,7 +1,18 @@
 use std::{env, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/healthcheck.proto")?;
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    let proto_files = vec![
+    ];
+
+    let proto_include = vec![
+        "proto"
+    ];
+
+    tonic_build::configure()
+        .compile_protos(&proto_files, &proto_include)
+        .unwrap_or_else(|e| panic!("Failed to compile protos , tonic: {:?}", e));
 
     //Inject proto files into the build script here
     Ok(())
--- generators/tonic-grpc/files/src/main.rs
+++ generators/tonic-grpc/files/src/main.rs
@@ -1,15 +1,23 @@
-use myapp::services::healthcheck::MyHealthCheck;
-use myapp::services::healthcheck::healthcheck::health_check_server::HealthCheckServer;
+mod services;
+use std::sync::LazyLock;
 use tonic::transport::Server;
+use tonic_health::server::HealthReporter;
+use prost_reflect::DescriptorPool;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let reflection_service = tonic_reflection::server::Builder::configure()
+        .include_reflection_service(true)
+        .build_v1()
+        .unwrap();
+
+    let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
+
     let addr = "[::1]:50051".parse().unwrap();
-
     println!("Server listening on {}", addr);
-
     Server::builder()
-        .add_service(HealthCheckServer::new(MyHealthCheck::default()))
+        .add_service(reflection_service)
+        .add_service(health_service)
         .serve(addr)
         .await?;
 
